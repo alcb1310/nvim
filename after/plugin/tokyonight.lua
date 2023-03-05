c3099e5f4235a957a63b4f3998ff04fa75041af3
Andrés Court <andres@andrescourt.com>
2023-03-04 19:09:38 -0500

    Added some config to the tonight theme

--- after/plugin/tokyonight.lua
+++ after/plugin/tokyonight.lua
@@ -1,2 +1,16 @@
 -- Lua
 vim.cmd([[colorscheme tokyonight]])
+
+require("tokyonight").setup({
+	style = "storm",
+	terminal_colors = true,
+	styles = {
+		comments = { italic = true },
+		keywords = { italic = true },
+		sidebars = "dark",
+		floats = "dark",
+	},
+	on_colors = function(colors)
+		colors.error = "#ff0000"
+	end,
+})
