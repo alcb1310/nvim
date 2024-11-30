c2d3a8076ca3171addf91d641d6a508976233a8e
Andres Court <andres@andrescourt.com>
2024-11-30 12:11:52 -0500

    Added some options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -3,3 +3,11 @@ local opt = vim.opt
 
 opt.relativenumber = true
 opt.number = true
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+
+opt.termguicolors = true
