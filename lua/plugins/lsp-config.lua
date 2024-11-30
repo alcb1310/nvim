01ead6a792626e4ef4969c8e6d9c8ff89395a896
Andres Court <andres@andrescourt.com>
2024-11-30 12:47:55 -0500

    Configured the lsp using, nvim-lspconfig and mason
    nvim-lspconfig:
        docs: https://github.com/neovim/nvim-lspconfig
    
    mason-lspconfig:
        docs: https://github.com/williamboman/mason-lspconfig.nvim
    
    mason:
        docs: https://github.com/williamboman/mason.nvim

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,67 @@
+local servers = {
+    "lua_ls",
+    "gopls",
+}
+
+local on_attach = function(client, bufnr)
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+                vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+        })
+    end
+
+    local opts = { buffer = bufnr, noremap = true, silent = true }
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set("n", "<leader>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+    vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+end
+
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            "williamboman/mason.nvim",
+            config = function()
+                require'mason'.setup()
+            end,
+        },
+
+        config = function()
+            require("mason-lspconfig").setup {
+                ensure_installed = servers,
+            }
+        end
+    },
+    config = function()
+        local lspconfig = require('lspconfig')
+
+        for _, lsp in ipairs(servers) do
+            lspconfig[lsp].setup {
+                on_attach = on_attach
+            }
+        end
+
+    end
+}
