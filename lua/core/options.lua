84fd0a35dd1df4fb0df1a22410cd210c30efdd74
Andres Court <andres@andrescourt.com>
2024-11-30 12:32:29 -0500

    FIX: use the proper syntax for comments

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,13 +1,20 @@
 vim.g.mapleader = " "
 local opt = vim.opt
 
+ -- Display linenumbers
 opt.relativenumber = true
 opt.number = true
 
+-- Tab configuration
 opt.tabstop = 4
 opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
 opt.smartindent = true
 
+-- Beter colors definition
 opt.termguicolors = true
+
+-- Search highlighting
+opt.hlsearch=false
+opt.incsearch = true
