1bf4676ac2019be4d8ba1ea599bf0c0b2baacf44
Andres Court <andres@andrescourt.com>
2024-11-30 23:32:54 -0500

    FIX: add silent remaps

--- lua/core/remaps.lua
+++ lua/core/remaps.lua
@@ -1,8 +1,19 @@
 local keymap = vim.keymap
+local opts = {
+    silent = true,
+    noremap = true
+}
 
-keymap.set("n", "<C-h>", "<C-w>h")
-keymap.set("n", "<C-j>", "<C-w>j")
-keymap.set("n", "<C-k>", "<C-w>k")
-keymap.set("n", "<C-l>", "<C-w>l")
+keymap.set("n", "<C-h>", "<C-w>h", opts)
+keymap.set("n", "<C-j>", "<C-w>j", opts)
+keymap.set("n", "<C-k>", "<C-w>k", opts)
+keymap.set("n", "<C-l>", "<C-w>l", opts)
 
-keymap.set("n", "-", ":Oil<cr>")
+keymap.set("n", "-", ":Oil<cr>", opts)
+
+keymap.set("v", "J", ":m '>+1<cr>gv=gv", opts)
+keymap.set("v", "K", ":m '<-2<cr>gv=gv", opts)
+
+keymap.set("n", "<A-v>", ":vsplit<cr>", opts)
+keymap.set("n", "<A-h>", ":split<cr>", opts)
+keymap.set("n", "<A-c>", ":close<cr>", opts)
