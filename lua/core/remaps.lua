51d7995bbef8bd469442adfa172e4718c82fb795
Andres Court <andres@andrescourt.com>
2024-12-03 21:39:40 -0500

    Oil show in a float window

--- lua/core/remaps.lua
+++ lua/core/remaps.lua
@@ -9,7 +9,7 @@ keymap.set("n", "<C-j>", "<C-w>j", opts)
 keymap.set("n", "<C-k>", "<C-w>k", opts)
 keymap.set("n", "<C-l>", "<C-w>l", opts)
 
-keymap.set("n", "-", ":Oil<cr>", opts)
+keymap.set("n", "-", ":Oil --float<cr>", opts)
 
 keymap.set("v", "J", ":m '>+1<cr>gv=gv", opts)
 keymap.set("v", "K", ":m '<-2<cr>gv=gv", opts)
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,10 +1,44 @@
 return {
-  'stevearc/oil.nvim',
-  cmd = { "Oil" },
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    'stevearc/oil.nvim',
+    cmd = { "Oil" },
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+        skip_confirm_for_simple_edits = true,
+        watch_for_changes = true,
+        float = {
+            -- Padding around the floating window
+            padding = 2,
+            max_width = 0,
+            max_height = 0,
+            border = "rounded",
+            win_options = {
+                winblend = 0,
+            },
+            -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+            get_win_title = nil,
+            -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+            preview_split = "right",
+            -- This is the config that will be passed to nvim_open_win.
+            -- Change values here to customize the layout
+            override = function(conf)
+                return conf
+            end,
+        },
+        preview_win = {
+            -- Whether the preview window is automatically updated when the cursor is moved
+            update_on_cursor_moved = true,
+            -- How to open the preview window "load"|"scratch"|"fast_scratch"
+            preview_method = "fast_scratch",
+            -- A function that returns true to disable preview on a file e.g. to avoid lag
+            disable_preview = function(filename)
+                return false
+            end,
+            -- Window-local options to use for preview window buffers
+            win_options = {},
+        },
+    },
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 }
