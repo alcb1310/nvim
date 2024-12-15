a560faa2d1fee2b53b744cef319df498f3449080
Andres Court <andres@andrescourt.com>
2024-12-14 19:43:17 -0500

    Disable notification on plugin changes

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     if vim.v.shell_error ~= 0 then
         vim.api.nvim_echo({
             { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
+            { out,                            "WarningMsg" },
             { "\nPress any key to exit..." },
         }, true, {})
         vim.fn.getchar()
@@ -34,5 +34,5 @@ require("lazy").setup({
     checker = { enabled = true },
     defaults = { lazy = true },
     rocks = { enable = false },
-    change_detection = { enable = false },
+    change_detection = { enable = false, notify = false },
 })
