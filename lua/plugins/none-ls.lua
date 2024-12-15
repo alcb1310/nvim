2485b92e08c3e1afbaa259493f904e05f4817bdd
Andres Court <andres@andrescourt.com>
2024-12-15 09:35:58 -0500

    added mason-null-ls for automatic linter and formatters installation

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,11 +1,37 @@
 return {
-    'nvimtools/none-ls.nvim',
-    event = { 'LspAttach' },
+    "nvimtools/none-ls.nvim",
+    event = { "LspAttach" },
     dependencies = {
-        'nvim-lua/plenary.nvim',
-        'nvimtools/none-ls-extras.nvim',
+        "nvim-lua/plenary.nvim",
+        "nvimtools/none-ls-extras.nvim",
+        {
+            "jay-babu/mason-null-ls.nvim",
+            event = { "BufReadPre", "BufNewFile" },
+            dependencies = {
+                "williamboman/mason.nvim",
+                "nvimtools/none-ls.nvim",
+            },
+        },
     },
     config = function()
+        require('mason-null-ls').setup({
+            ensure_installed = {
+                'eslint_d',
+                'gofumpt',
+                'goimports-reviser',
+                'golangci-lint',
+                'prettierd',
+                'stylua',
+            },
+            methods = {
+                diagnostics = true,
+                formatting = true,
+                code_actions = true,
+                completion = true,
+                hover = true,
+            },
+        })
+
         local null_ls = require("null-ls")
 
         null_ls.setup({
@@ -15,10 +41,10 @@ return {
                 null_ls.builtins.formatting.goimports_reviser,
                 null_ls.builtins.formatting.gofumpt,
                 null_ls.builtins.formatting.prettierd,
-                -- require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
+
                 null_ls.builtins.diagnostics.golangci_lint,
-                require 'none-ls.code_actions.eslint',
+                require("none-ls.code_actions.eslint"),
             },
         })
-    end
+    end,
 }
