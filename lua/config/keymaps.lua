df36fc4e10ab8f70f7a5bdbbc938083e13791bd3
Andres Court <andres@andrescourt.com>
2024-12-17 20:42:00 -0500

    added more keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 vim.g.mapleader = ' '
 
-vim.keymap.set('n', '-', vim.cmd.Oil, { desc = 'Open file explorer' } )
+vim.keymap.set('n', '-', vim.cmd.Oil, { desc = 'Open file explorer' })
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
