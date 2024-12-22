99dab6200efd2af9aa8b75eebc8fe4d2f467a4ab
Andres Court <andres@andrescourt.com>
2024-12-21 20:15:30 -0500

    Changes the blink.nvim version

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -4,7 +4,8 @@ return {
     dependencies = 'rafamadriz/friendly-snippets',
 
     -- use a release tag to download pre-built binaries
-    version = 'v0.*',
+    version = 'v0.7.6',
+    -- version = 'v0.*',
     -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
     -- build = 'cargo build --release',
     -- If you use nix, you can build from source using latest nightly rust with:
