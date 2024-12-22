1421996b409b02af16087c44b1346ef27b226d74
Andres Court <andres@andrescourt.com>
2024-12-21 20:17:06 -0500

    added keymaps for neotest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -6,23 +6,32 @@ return {
         "antoinemadec/FixCursorHold.nvim",
         "nvim-neotest/neotest-go",
     },
+    cmd = { "Neotest" },
     keys = {
-        { "<leader>tf", function()
-            require("neotest").run.run(vim.fn.expand("%"))
-        end },
+        {
+            "<leader>tf",
+            function()
+                require("neotest").run.run(vim.fn.expand("%"))
+            end,
+        },
 
-        { "<leader>tr", function()
-            require("neotest").run.run()
-        end },
+        {
+            "<leader>tr",
+            function()
+                require("neotest").run.run()
+            end,
+        },
+        { "<leader>ts", ":Neotest summary<cr>", { silent = true } },
+        { "<leader>to", ":Neotest output<cr>", { silent = true } },
     },
     config = function()
         ---@diagnostic disable-next-line: missing-fields
         require("neotest").setup({
             adapters = {
                 require("neotest-go")({
-                    recursive_run = true
-                })
-            }
+                    recursive_run = true,
+                }),
+            },
         })
-    end
+    end,
 }
