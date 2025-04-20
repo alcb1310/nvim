8a6b9dc9bb93b372585f6367ca2f9200a8dfc8da
Andres Court <andres@andrescourt.com>
2025-04-19 21:31:13 -0500

    feat(optios): added some initial editor options

---
+++ lua/core/options.lua
@@ -0,0 +1,20 @@
+local opt = vim.opt
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+opt.number = true
+opt.relativenumber = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Keep signcolumn on by default
+opt.signcolumn = "yes"
+
+-- Set 2 spaces for tab
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
