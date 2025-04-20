31180bfb0ae9643dbaf8f819cb50d37712d688cb
Andres Court <andres@andrescourt.com>
2025-04-20 10:16:35 -0500

    feat(neotes): added plugin

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,43 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-neotest/neotest-go",
+    "nvim-neotest/neotest-jest",
+    "marilari88/neotest-vitest",
+  },
+  cmd = { "Neotest" },
+  keys = {
+    {
+      "<leader>tf",
+      function()
+        require("neotest").run.run(vim.fn.expand("%"))
+      end,
+    },
+
+    {
+      "<leader>tr",
+      function()
+        require("neotest").run.run()
+      end,
+    },
+    { "<leader>ts", ":Neotest summary<cr>", { silent = true } },
+    { "<leader>to", ":Neotest output<cr>",  { silent = true } },
+  },
+  config = function()
+    ---@diagnostic disable-next-line: missing-fields
+    require("neotest").setup({
+      adapters = {
+        require("neotest-go")({
+          recursive_run = true,
+        }),
+        require("neotest-jest")({
+          jestCommand = "pnpm test --",
+        }),
+        require("neotest-vitest")
+      },
+    })
+  end,
+}
