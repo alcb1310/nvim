3ac1b2174f4e150d58a8fe26cdad283dd1d6a96c
Andres Court <andres@andrescourt.com>
2025-04-19 22:20:16 -0500

    feat(picker): installed fzf-lua

---
+++ lua/plugins/fzf.lua
@@ -0,0 +1,16 @@
+return {
+  "ibhagwan/fzf-lua",
+  -- optional for icon support
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  -- or if using mini.icons/mini.nvim
+  -- dependencies = { "echasnovski/mini.icons" },
+  opts = {},
+  cmd = 'FzfLua',
+  keys = {
+    { '<leader>sf', '<cmd>FzfLua files<cr>', desc= '[S]earch [F]iles'},
+    { '<leader>sg', '<cmd>FzfLua live_grep<cr>', desc= '[S]earch [G]rep'},
+    { '<leader>sh', '<cmd>FzfLua help_tags<cr>', desc= '[S]earch [H]elp'},
+    { '<leader>gf', '<cmd>FzfLua git_files<cr>', desc= '[G]it [F]iles'},
+    { '<leader>gb', '<cmd>FzfLua git_blame<cr>', desc= '[G]it [B]lame'},
+  },
+}
