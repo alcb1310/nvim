a4c9198d0be2dcc89ea9d5276cf3cf3c3405e3b9
Andres Court <andres@andrescourt.com>
2025-04-20 00:56:19 -0500

    feat(gitsigns): added keymaps

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -51,5 +51,45 @@ return {
         col = 1
       },
     }
+
+    local gitsigns = require('gitsigns')
+    --- @param mode string
+    --- @param l string
+    --- @param r string | function
+    --- @param opts vim.keymap.set.Opts?
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Actions
+    map('n', '<leader>hs', gitsigns.stage_hunk)
+    map('n', '<leader>hr', gitsigns.reset_hunk)
+
+    map('v', '<leader>hs', function()
+      gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    end)
+
+    map('v', '<leader>hr', function()
+      gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    end)
+
+    map('n', '<leader>hS', gitsigns.stage_buffer)
+    map('n', '<leader>hR', gitsigns.reset_buffer)
+    map('n', '<leader>hp', gitsigns.preview_hunk)
+    map('n', '<leader>hi', gitsigns.preview_hunk_inline)
+
+    map('n', '<leader>hb', function()
+      gitsigns.blame_line({ full = true })
+    end)
+
+    map('n', '<leader>hd', gitsigns.diffthis)
+
+    map('n', '<leader>hD', function()
+      gitsigns.diffthis('~')
+    end)
+
+    map('n', '<leader>hQ', function() gitsigns.setqflist('all') end)
+    map('n', '<leader>hq', gitsigns.setqflist)
   end
 }
