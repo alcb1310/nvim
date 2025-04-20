38cb794411cea78e10275025a62ab77ab4b0b9c2
Andres Court <andres@andrescourt.com>
2025-04-20 01:17:19 -0500

    feat(conform): biome setup

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,14 +1,21 @@
 return {
   'stevearc/conform.nvim',
   opts = {},
-  config = function ()
+  config = function()
     require("conform").setup({
+      formatters = {
+        biome = {
+          require_cwd = true,
+        },
+      },
       formatters_by_ft = {
         lua = { "stylua" },
         go = {
-          "gofumpt", -- Install go install mvdan.cc/gofumpt@latest
+          "gofumpt",           -- Install go install mvdan.cc/gofumpt@latest
           "goimports-reviser", -- Install go install -v github.com/incu6us/goimports-reviser/v3@latest
-        }
+        },
+        javascript = { "biome" },
+        typescript = { "biome", "prettier", stop_after_first = true },
       },
       format_on_save = {
         timeout_ms = 500,
