96cce49916ad22ca5bbe6317b6d9297b963bd0a3
Andres Court <andres@andrescourt.com>
2025-04-20 22:24:15 -0500

    feat(emmet_ls): added emmet lsp

---
+++ lsp/emmet_ls.lua
@@ -0,0 +1,9 @@
+-- Install with
+-- npm install -g emmet-ls
+
+---@type vim.lsp.Config
+return {
+  cmd = { "emmet-ls", "--stdio" },
+  filetypes = { "astro", "css", "eruby", "html", "htmldjango", "javascriptreact", "less", "pug", "sass", "scss", "svelte", "typescriptreact", "vue", "htmlangular" },
+  root_markers = { ".git" }
+}
