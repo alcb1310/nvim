59b805aff5badbf1bc2636125e7a6d564456260c
Andres Court <andres@andrescourt.com>
2025-04-20 22:47:50 -0500

    feat(which key): installed plugin

--- init.lua
+++ init.lua
@@ -34,4 +34,12 @@ require("lazy").setup({
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
+  change_detection = { notify = false },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "netrwPlugin",
+      }
+    }
+  }
 })
