9b98f17d750255469b3190413f9e53b3746b25c7
Andres Court <andres@andrescourt.com>
2025-04-25 21:04:37 -0500

    feat(fzf): added a new keymap

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,16 +1,17 @@
 return {
   "ibhagwan/fzf-lua",
   -- optional for icon support
-  dependencies = {{ "echasnovski/mini.icons", opts = {} }},
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- or if using mini.icons/mini.nvim
   -- dependencies = { "echasnovski/mini.icons" },
   opts = {},
   cmd = 'FzfLua',
   keys = {
-    { '<leader>sf', '<cmd>FzfLua files<cr>', desc= '[S]earch [F]iles'},
-    { '<leader>sg', '<cmd>FzfLua live_grep<cr>', desc= '[S]earch [G]rep'},
-    { '<leader>sh', '<cmd>FzfLua help_tags<cr>', desc= '[S]earch [H]elp'},
-    { '<leader>gf', '<cmd>FzfLua git_files<cr>', desc= '[G]it [F]iles'},
-    { '<leader>gb', '<cmd>FzfLua git_blame<cr>', desc= '[G]it [B]lame'},
+    { '<leader>sf', '<cmd>FzfLua files<cr>',     desc = '[S]earch [F]iles' },
+    { '<leader>sb', '<cmd>FzfLua buffers<cr>',   desc = '[S]earch [B]uffers' },
+    { '<leader>sg', '<cmd>FzfLua live_grep<cr>', desc = '[S]earch [G]rep' },
+    { '<leader>sh', '<cmd>FzfLua help_tags<cr>', desc = '[S]earch [H]elp' },
+    { '<leader>gf', '<cmd>FzfLua git_files<cr>', desc = '[G]it [F]iles' },
+    { '<leader>gb', '<cmd>FzfLua git_blame<cr>', desc = '[G]it [B]lame' },
   },
 }
