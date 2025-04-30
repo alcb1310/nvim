adeddfbe120ceb23d1ced361f1e64947ce6d8ae5
Andres Court <andres@andrescourt.com>
2025-04-29 21:16:33 -0500

    feat(colorscheme): added the kanagawa colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,30 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    require'tokyonight'.setup{
-      style = "night",
-      transparent = true,
-      terminal_colors = false,
-    }
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      require 'tokyonight'.setup {
+        style = "night",
+        transparent = true,
+        terminal_colors = false,
+      }
 
-    vim.cmd[[colorscheme tokyonight]]
-  end,
+      -- vim.cmd [[colorscheme tokyonight]]
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      require 'kanagawa'.setup {
+        transparent = true,
+      }
+
+      vim.cmd [[colorscheme kanagawa]]
+    end,
+  }
 }
