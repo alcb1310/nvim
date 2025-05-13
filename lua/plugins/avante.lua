44749c3181c779b769d41ae2bf6244e7516224b1
Andres Court <andres@andrescourt.com>
2025-05-12 21:59:26 -0500

    fix(avante): remove unnecesary dependency telescope

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -146,11 +146,10 @@ return {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
-    "echasnovski/mini.pick",         -- for file_selector provider mini.pick
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    "ibhagwan/fzf-lua",              -- for file_selector provider fzf
-    "echasnovski/mini.nvim",         -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua",        -- for providers='copilot'
+    "echasnovski/mini.pick",  -- for file_selector provider mini.pick
+    "ibhagwan/fzf-lua",       -- for file_selector provider fzf
+    "echasnovski/mini.nvim",  -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
