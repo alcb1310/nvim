09c3d0c1b0d721fe821b7a3beda4395865eaeb4a
Andres Court <andres@andrescourt.com>
2025-05-12 22:00:27 -0500

    feat(keymaps): added quality of life plugins

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -9,3 +9,15 @@ vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to the left window', remap
 vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to the bottom window', remap = true })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to the top window', remap = true })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to the right window', remap = true })
+
+-- next greatest remap ever : asbjornHaland
+-- press <leader>y to yank text to the clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- this keymap adds an error nil check for golang
+vim.keymap.set(
+  "n",
+  "<leader>ee",
+  "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+)
