7d81192cead44374162380cdea2ea67a740f61e9
Andres Court <andres@andrescourt.com>
2025-08-14 18:10:58 -0500

    feat: added lsp for lua language server

--- init.lua
+++ init.lua
@@ -22,11 +22,43 @@ vim.o.expandtab = true -- replace tab characters with the propper amount of spac
 vim.o.incsearch = true -- Highlight the text being searched
 vim.o.hlsearch = false -- Remove highlight when search has finished
 
+-- Sign column
+vim.o.signcolumn = 'yes'
+
 -- Completion options
 vim.o.completeopt = 'fuzzy,menuone,noselect,popup'
+vim.o.winborder = 'rounded'
 
 -- Here we will be defining some global keymaps
 
 vim.keymap.set('n', '-', vim.cmd.Ex, { desc = 'Open netrw file browser' })
 vim.keymap.set('n', '<leader>so', '<cmd>update<cr><cmd>source<cr>', { desc = 'saves and sources the current file', silent = true })
 vim.keymap.set('i', '<M-i>', '<C-x><C-o>', { desc = 'Displays completion menu' })
+
+-- Here we will be setting up our LSP servers
+vim.lsp.enable({
+    'lua_ls',
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    desc = 'LSP specific configuration and keymaps',
+    group = vim.api.nvim_create_augroup('alcb1310/lsp', { clear = true }),
+    callback = function (ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if not client then
+            return
+        end
+
+        vim.lsp.completion.enable(true, client.id, 0, {autotrigger = true} )
+
+        vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, {desc = 'Format current file' })
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to Definition'})
+        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code Actions' })
+    end
+})
+
+vim.diagnostic.config({
+    virtual_lines = {
+        current_line = true
+    }
+})
