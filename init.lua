256035cd315155a49c0203bc536ce3836f3817de
Andres Court <andres@andrescourt.com>
2025-08-15 12:11:44 -0500

    create autocommand to create directories if needed

--- init.lua
+++ init.lua
@@ -61,3 +61,14 @@ vim.api.nvim_create_autocmd('BufWritePre', {
         vim.lsp.buf.format()
     end
 })
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+    desc = 'Create non existent directories if needed',
+    group = vim.api.nvim_create_augroup('alcb1310/directory', { clear = true }),
+    callback = function()
+        local dir = vim.fn.expand('<afile>:p:h')
+        if vim.fn.isdirectory(dir) == 0 then
+            vim.fn.mkdir(dir, 'p')
+        end
+    end
+})
