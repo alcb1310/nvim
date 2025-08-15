91d52ad43fe5393be036ec7ea02ab91f36a51a44
Andres Court <andres@andrescourt.com>
2025-08-15 12:26:57 -0500

    open buffer in last position

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -16,3 +16,16 @@ vim.api.nvim_create_autocmd('BufWritePre', {
         end
     end
 })
+
+vim.api.nvim_create_autocmd('BufReadPost', {
+    desc = 'Open buffer in last position',
+    group = vim.api.nvim_create_augroup('alcb1310/open', { clear = true }),
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end
+})
