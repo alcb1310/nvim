c61cb996fec3e84260cc0638bf94b56958c96553
Andres Court <andres@andrescourt.com>
2025-08-15 12:37:36 -0500

    add quality of life options and reorder some settings

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,8 +1,12 @@
 -- Here we will be defining some Neovim configuration options
 
--- Line numbering
+-- Basic settings
 vim.o.number = true         -- shows line numbers
 vim.o.relativenumber = true -- shows line number relative to the current line
+vim.o.cursorline = true
+vim.o.cursorcolumn = true
+vim.o.wrap = false
+vim.o.scrolloff = 10
 
 -- Tab behaviour
 vim.o.tabstop = 4        -- number of spaces a `tab` stands for
@@ -15,11 +19,16 @@ vim.o.expandtab = true   -- replace tab characters with the propper amount of sp
 vim.o.incsearch = true -- Highlight the text being searched
 vim.o.hlsearch = false -- Remove highlight when search has finished
 
--- Sign column
-vim.o.signcolumn = 'yes'
-
 -- Completion options
 vim.o.completeopt = 'fuzzy,menuone,noselect,popup'
 vim.o.winborder = 'rounded'
+vim.o.pumheight = 15
+vim.o.pumblend = 7
 
-vim.opt.path:append('**')
+-- Visual settings
+vim.o.termguicolors = true
+vim.o.colorcolumn = '100'
+
+-- Behavior settings
+vim.opt.path:append('**') -- Include subdirectories when using the find command
+vim.o.signcolumn = 'yes'
