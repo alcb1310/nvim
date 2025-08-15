120ab41ae40c6c8ff1a93e2ada218c830327529a
Andres Court <andres@andrescourt.com>
2025-08-15 12:13:31 -0500

    move the autocommands to its own file

--- init.lua
+++ init.lua
@@ -46,22 +46,3 @@ vim.diagnostic.config({
         current_line = true
     }
 })
-
-vim.api.nvim_create_autocmd('BufWritePre', {
-    desc = 'Format files on save',
-    group = vim.api.nvim_create_augroup('alcb1310/save', { clear = true }),
-    callback = function()
-        vim.lsp.buf.format()
-    end
-})
-
-vim.api.nvim_create_autocmd('BufWritePre', {
-    desc = 'Create non existent directories if needed',
-    group = vim.api.nvim_create_augroup('alcb1310/directory', { clear = true }),
-    callback = function()
-        local dir = vim.fn.expand('<afile>:p:h')
-        if vim.fn.isdirectory(dir) == 0 then
-            vim.fn.mkdir(dir, 'p')
-        end
-    end
-})
