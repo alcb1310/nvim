9b385fa0c6ce71a2240b7fecad6cc455b07a4b5b
Andres Court <andres@andrescourt.com>
2025-09-05 15:41:55 -0500

    migrate harpoon to harpoon2

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,18 +1,17 @@
-local harpoon = require 'harpoon'
+local harpoon = require("harpoon")
 
-harpoon.setup()
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
 
-local mark = require 'harpoon.mark'
-local ui = require 'harpoon.ui'
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set('n', '<leader>a', mark.add_file, { desc = 'Adds a file to harpoon', silent = true })
-vim.keymap.set('n', '<c-e>', ui.toggle_quick_menu, { desc = 'Shows harpoon menu', silent = true })
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
 
-vim.keymap.set('n', '<leader>h', function() ui.nav_file(1) end,
-    { desc = 'navigates to first harpoon file', silent = true })
-vim.keymap.set('n', '<leader>j', function() ui.nav_file(2) end,
-    { desc = 'navigates to second harpoon file', silent = true })
-vim.keymap.set('n', '<leader>k', function() ui.nav_file(3) end,
-    { desc = 'navigates to third harpoon file', silent = true })
-vim.keymap.set('n', '<leader>l', function() ui.nav_file(4) end,
-    { desc = 'navigates to fourth harpoon file', silent = true })
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,7 +4,7 @@ vim.pack.add({
     { src = "https://github.com/nvim-lua/plenary.nvim" },
     { src = "https://github.com/nvim-neotest/neotest" },
     { src = "https://github.com/nvim-neotest/nvim-nio" },
-    { src = 'https://github.com/ThePrimeagen/harpoon' },
+    { src = 'https://github.com/ThePrimeagen/harpoon',                   version = 'harpoon2' },
     { src = 'https://github.com/folke/flash.nvim' },
     { src = 'https://github.com/folke/todo-comments.nvim' },
     { src = 'https://github.com/ibhagwan/fzf-lua' },
