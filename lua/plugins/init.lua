9fe21d37223b7544dc6c1a0bbac95f518e64c23b
Andres Court <andres@andrescourt.com>
2025-09-07 09:59:45 -0500

    added vim fugitive

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -21,6 +21,7 @@ vim.pack.add({
     { src = 'https://github.com/rcarriga/nvim-dap-ui' },
     { src = 'https://github.com/rebelot/kanagawa.nvim' },
     { src = 'https://github.com/theHamsta/nvim-dap-virtual-text' },
+    { src = 'https://github.com/tpope/vim-fugitive' },
 })
 
 vim.api.nvim_create_autocmd('PackChanged', {
