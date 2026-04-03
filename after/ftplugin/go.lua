4ffda83d7658ed83ccc4d943859271e28a151d74
Andres Court <andres@andrescourt.com>
2026-04-02 22:57:44 -0500

    load neotest plugin only with go files

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,3 +1,12 @@
+vim.pack.add({
+    { src = "https://github.com/fredrikaverpil/neotest-golang" },
+    { src = "https://github.com/nvim-neotest/neotest" },
+    { src = "https://github.com/nvim-neotest/nvim-nio" },
+    { src = "https://github.com/antoinemadec/FixCursorHold.nvim" },
+    { src = "https://github.com/nvim-lua/plenary.nvim" },
+    { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
+})
+
 local test = require 'neotest'
 
 test.setup({
--- plugin/neo-test.lua
+++
@@ -1,8 +0,0 @@
-vim.pack.add({
-    { src = "https://github.com/fredrikaverpil/neotest-golang" },
-    { src = "https://github.com/nvim-neotest/neotest" },
-    { src = "https://github.com/nvim-neotest/nvim-nio" },
-    { src = "https://github.com/antoinemadec/FixCursorHold.nvim" },
-    { src = "https://github.com/nvim-lua/plenary.nvim" },
-    { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
-})
