30b71c05bb21f2c666e4c7db6f263d99dbc135ea
Andres Court <andres@andrescourt.com>
2026-04-02 19:15:14 -0500

    update some keymaps for more confort

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,21 +5,21 @@ vim.keymap.set('n', '<leader>so', '<cmd>update<cr><cmd>source<cr>',
     { desc = 'saves and sources the current file', silent = true })
 vim.keymap.set('i', '<M-i>', '<C-x><C-o>', { desc = 'Displays completion menu' })
 vim.keymap.set('n', '<leader>rs', '<cmd>update<cr><cmd>restart<cr>', { desc = 'saves and restart neovim', silent = true })
-vim.keymap.set({ 'i', 'v' }, 'jk', '<Esc>', { desc = 'Escape', silent = true })
-vim.keymap.set({ 'i', 'v' }, 'kj', '<Esc>', { desc = 'Escape', silent = true })
+vim.keymap.set({ 'i', 'v' }, 'fd', '<Esc>', { desc = 'Escape', silent = true })
+vim.keymap.set({ 'i', 'v' }, 'df', '<Esc>', { desc = 'Escape', silent = true })
 
 vim.keymap.set('n', '<leader>f', ':find  **/*')
 vim.keymap.set('n', '<leader>b', ':b ')
 
 vim.keymap.set('n', 'n', 'nzz', { desc = 'Find next result and center' })
 vim.keymap.set('n', 'N', 'Nzz', { desc = 'Find prev result and center' })
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down and center' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up and center' })
 
-vim.keymap.set('n', '<C-h>', '<C-w>h')
-vim.keymap.set('n', '<C-j>', '<C-w>j')
-vim.keymap.set('n', '<C-k>', '<C-w>k')
-vim.keymap.set('n', '<C-l>', '<C-w>l')
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to left window' })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to down window' })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to up window' })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to right window' })
 
 vim.keymap.set('n', '<leader>m', '<cmd>make<cr>', { desc = '[M]ake', silent = true })
 vim.keymap.set('n', '<leader>mr', '<cmd>make run<cr>', { desc = '[M]ake [R]un', silent = true })
@@ -33,4 +33,7 @@ vim.keymap.set('n', '<leader>Y', [["+Y]], { desc = 'Yank to the system\'s clipbo
 
 vim.keymap.set('n', 'x', '"_x', { desc = 'Use unknown register when deleting characters', silent = true })
 
-vim.keymap.set('n', '<leader>pu', vim.pack.update, { desc = 'update packages', silent = true })
+vim.keymap.set('n', '<leader>pu', vim.pack.update, { desc = '[P]ack [U]pdate', silent = true })
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
