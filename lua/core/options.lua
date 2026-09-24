c9a624d2e0b30a2237de066f505edd44fd8f9cc1
Andres Court <andres@andrescourt.com>
2026-09-24 16:45:54 -0500

    feat(options): syntax highlighting and spelling

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -9,3 +9,9 @@ vim.o.scrolloff = 8           -- Number of lines visible when scrolling
 vim.o.wrap = false            -- Never wrap lines of text
 vim.o.number = true           -- Show line numbers
 vim.o.relativenumber = true   -- Show realtive line numbers
+
+-- 5. syntax highlighting and spelling
+vim.o.hlsearch = false        -- Remove highlight when search has finished
+vim.o.termguicolors = true    -- Enhanced colors
+vim.o.cursorline = true       -- Highlight current line for easier visualization
+vim.o.colorcolumn = '100'     -- Highlight the specified column as a reference
