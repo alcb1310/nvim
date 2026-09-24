85896cf46e78a250a0b9c399215f683cabf7f9af
Andres Court <andres@andrescourt.com>
2026-09-24 17:52:14 -0500

    feat(autocommands): format file on save

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -1,7 +1,15 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight on yank',
-    group = vim.api.nvim_create_augroup('alcb1310/yank', { clear = true } ),
+    group = vim.api.nvim_create_augroup('alcb1310/yank', { clear = true }),
     callback = function()
         vim.hl.hl_op()
+    end
+})
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+    desc = 'Format files on save',
+    group = vim.api.nvim_create_augroup('alcb1310/save', { clear = true }),
+    callback = function()
+        vim.lsp.buf.format()
     end
 })
