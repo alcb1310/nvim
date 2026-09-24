90b35dbdeb10bdc4c10d45ee8b754abdeca06371
Andres Court <andres@andrescourt.com>
2026-09-24 16:42:51 -0500

    feat(options): added comments on what is the option is doing

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -5,7 +5,7 @@
 -- 3. tags
 
 -- 4. displaying text
-vim.o.scrolloff = 8
-vim.o.wrap = false
-vim.o.number = true
-vim.o.relativenumber = true
+vim.o.scrolloff = 8           -- Number of lines visible when scrolling
+vim.o.wrap = false            -- Never wrap lines of text
+vim.o.number = true           -- Show line numbers
+vim.o.relativenumber = true   -- Show realtive line numbers
