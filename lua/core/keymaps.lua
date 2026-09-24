3b2efe45f3c980e9637237ce9078122649e3767d
Andres Court <andres@andrescourt.com>
2026-09-24 17:25:37 -0500

    feat(keymaps): yank to system clipboard

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -13,3 +13,7 @@ vim.keymap.set('n', 'n', 'nzz', { desc = 'Find next result and center' } )
 vim.keymap.set('n', 'N', 'Nzz', { desc = 'Find previous result and center' } )
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down and center' } )
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up and center' } )
+
+-- Yank to the system's clipboard
+vim.keymap.set({'n', 'v'}, '<leader>y', [["+y]], { silent = true } )
+vim.keymap.set('n', '<leader>Y', [["+Y]], { silent = true } )
